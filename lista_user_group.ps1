de8920941b9854f5dea0b4a1daf83f8a81af0a02
douglasurbano <douglas.urbano@outlook.com>
2013-07-02 18:14:02 -0300

    Update lista_user_group.ps1

--- lista_user_group.ps1
+++ lista_user_group.ps1
@@ -1,6 +1,6 @@
 Import-Module activedirectory
 
-$usuarios = get-adUser -filter * -SearchBase "DC=lab,dc=nexa,dc=com,dc=br" -Properties * # | ft DisplayName, SamAccountName,Enabled,MemberOf
+$usuarios = get-adUser -filter * -SearchBase "DC=dominio,dc=local" -Properties * # | ft DisplayName, SamAccountName,Enabled,MemberOf
 
 foreach($u in $usuarios){
     "==============================================================="
@@ -9,8 +9,8 @@ foreach($u in $usuarios){
     "Habilitado: "+ $u.Enabled
     "Grupos    :"
     foreach ($g in $u.memberof){
-        #Aqui coloquei duas opcoes de nome de grupo
-        #Basta voce apagar a linha que vc nao quer
+        #Aqui existe duas opcoes de nome de grupo
+        #Basta apagar a linha que vc nao quer
         "`t"+$g  #Aqui mostra o nome distinto
         "`t"+(Get-ADGroup $g).name  #Aqui mostra o nome do grupo
     }
